e2e01eb53bf1de2ac81999d60be2cd7c4c23095a
jtrieu1998 <43070404+jtrieu1998@users.noreply.github.com>
2022-12-06 00:26:39 -0600

    day 4 part 2

--- 4/solution.js
+++ 4/solution.js
@@ -15,13 +15,37 @@ inputArray.forEach((pairs, index) => {
 
   if(first[0]<=second[0] && first[1]>=second[1]){
     fullCotains++
-    console.log(first,second)
   } else if(second[0]<=first[0] && second[1]>=first[1]){
     fullCotains++
-    console.log(first,second)
   }
 })
 
 console.log("Part 1:",fullCotains)
 
+let a,b,c,d = 0
+fullCotains = 0
+inputArray.forEach((pairs, index) => {
+  pairs = pairs.split(",")
+  first = pairs[0].split("-").map(Number)
+  second = pairs[1].split("-").map(Number)
+  // [ a, b ] = first
+  // [ c, d ] = second
 
+  a = first[0]
+  b = first[1]
+  c = second[0]
+  d = second[1]
+  if(a<=c && b >= d){
+    fullCotains++
+    // console.log(first,second)
+  } else if(c<=a && d>=b){
+    fullCotains++
+    // console.log(first,second)
+  } else if(a <= c && b >= c){
+    fullCotains++
+  } else if(c <= a && d >= a){
+    fullCotains++
+  }
+})
+
+console.log("Part 2:",fullCotains)
